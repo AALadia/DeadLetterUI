06c96b011a519e533cc1b599990d6db520e4e2d6
Adrian Anton Ladia <ladia.adrian@gmail.com>
2025-08-14 15:22:15 +0800

    Add dead letter test and update PubSub handler
    Added test_deadletter.py to test publishing messages to the createSalesOrder topic using service account credentials. Improved formatting and error handling in the createDeadLetter endpoint in appPubSub.py. Updated .gitignore to include new service account key.

--- server/appPubSub.py
+++ server/appPubSub.py
@@ -10,12 +10,13 @@ logging.basicConfig(level=logging.INFO)
 
 
 @main.route('/createDeadLetter', methods=['POST'])
-
 def createDeadLetter():
     if request.is_json:
         data = request.get_json()
-        if isinstance(data["message"]["data"],str):
-             data["message"]["data"] = PubSub().decodeMessage(data["message"]["data"])
+        print(data)
+        if isinstance(data["message"]["data"], str):
+            data["message"]["data"] = PubSub().decodeMessage(
+                data["message"]["data"])
         _id = data["message"]["data"].get('_id')
         originalMessage = data["message"]["data"].get('originalMessage')
         topicName = data["message"]["data"].get('topicName')
@@ -23,18 +24,29 @@ def createDeadLetter():
         endpoint = data["message"]["data"].get('endpoint')
         errorMessage = data["message"]["data"].get('errorMessage')
         publisherProjectId = data["message"]["data"].get('publisherProjectId')
-        publisherProjectName = data["message"]["data"].get('publisherProjectName')
+        publisherProjectName = data["message"]["data"].get(
+            'publisherProjectName')
         topicName = data["message"]["attributes"].get("topicName")
         subscriberName = data["subscription"]
         endpoint = ""
         errorMessage = ""
-        publisherProjectId = data["message"]["attributes"].get("publisherProjectId")
-        publisherProjectName = data["message"]["attributes"].get("publisherProjectName")
+        publisherProjectId = data["message"]["attributes"].get(
+            "publisherProjectId")
+        publisherProjectName = data["message"]["attributes"].get(
+            "publisherProjectName")
         originalMessage = data["message"]["data"]
     try:
-        res = PubSubRequests().createDeadLetter(_id, originalMessage, topicName, subscriberName, endpoint, errorMessage, publisherProjectId, publisherProjectName)
+        res = PubSubRequests().createDeadLetter(_id, originalMessage,
+                                                topicName, subscriberName,
+                                                endpoint, errorMessage,
+                                                publisherProjectId,
+                                                publisherProjectName)
     except Exception as e:
         traceback.print_exc()
-        return jsonify({'message': str(e),'data':None,'status':400}), 400
+        return jsonify({'message': str(e), 'data': None, 'status': 400}), 400
 
-    return jsonify({"message": "Dead letter message created successfully", "status":200, "data": res}), 200
+    return jsonify({
+        "message": "Dead letter message created successfully",
+        "status": 200,
+        "data": res
+    }), 200
