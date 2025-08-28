5403f4dff8848cbd551e1b0ef1ab183e78f59ac3
Adrian Anton Ladia <ladia.adrian@gmail.com>
2025-08-28 13:37:22 +0800

    Update jobDeleteAllITestDeadLetters.py

--- server/jobDeleteAllITestDeadLetters.py
+++ server/jobDeleteAllITestDeadLetters.py
@@ -2,6 +2,12 @@ from mongoDb import db
 
 def delete_all_test_dead_letters():
     db.delete({'originalMessage.isTestForAppDoNotDelete':True},'DeadLetters')
+    # try:
+    db.delete({'originalMessage.customer':'test1'},'DeadLetters')
+    db.delete({'originalMessage.customer':'test2'},'DeadLetters')
+    # except:
+    #     pass
+
 
 if __name__ == '__main__':
     delete_all_test_dead_letters()
