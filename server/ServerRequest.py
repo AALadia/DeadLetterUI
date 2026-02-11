e3adbf3079c5d1455a06478c7576eda3a89aa237
Adrian Anton Ladia <ladia.adrian@gmail.com>
2026-02-12 07:29:08 +0800

    Update ServerRequest.py

--- server/ServerRequest.py
+++ server/ServerRequest.py
@@ -1,8 +1,10 @@
 import requests
+import traceback
 from urllib.parse import urljoin
 
 
 class ServerRequest:
+
     def __init__(self, serverBaseUrl, headers):
         self.serverBaseUrl = serverBaseUrl
         self.headers = headers
@@ -10,9 +12,13 @@ class ServerRequest:
     def post(self, path, payload):
         url = urljoin(self.serverBaseUrl, path)
         try:
-            response = requests.post(url, json=payload, headers=self.headers,timeout=5)
+            response = requests.post(url,
+                                     json=payload,
+                                     headers=self.headers,
+                                     timeout=5)
             response.raise_for_status(
             )  # Raises an HTTPError for bad responses
             return response.json()
         except Exception as e:
-            raise ValueError(str(e))
+            tb = traceback.format_exc()
+            raise ValueError(f"{str(e)}\nTraceback:\n{tb}")
